2ee136e7e621f28297a8956563a45650e6ef4514
Kristen Clunie <kcluniesa@yahoo.com>
2019-04-06 22:36:46 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog
 
 attr_accessor :name, :breed, :age 
 
-@all = []
+@@all = []
 
 def initialize(name, breed, age)
   @name= name
@@ -11,7 +11,7 @@ def initialize(name, breed, age)
 end
 
 def self.all 
-  @all 
+  @@all 
 end
   
 end
